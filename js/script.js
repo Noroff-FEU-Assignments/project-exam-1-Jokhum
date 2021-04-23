e4dc88da20cefbf29aaa6a4eab2cee7391e11da6
Jokhum <p.werven@gmail.com>
2021-04-23 20:30:19 +0200

    More html / css and a littlebit js for burger

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const hamburger = document.getElementById('hamburger');
+
+const navBar = document.getElementById('nav-bar');
+
+hamburger.addEventListener('click', () => {
+
+    navBar.classList.toggle('show');
+
+});
